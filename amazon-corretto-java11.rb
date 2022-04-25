475d1c2ec323365db7fb5893abf4a8bc458a6d72
Evan Niessen-Derry <evan.niessen-derry@shortcut.com>
2022-04-25 12:03:04 -0500

    add Amazon Corretto Java11

---
+++ amazon-corretto-java11.rb
@@ -0,0 +1,22 @@
+cask "amazon-corretto-java11" do
+# This is a fork of https://github.com/Homebrew/homebrew-cask/blob/HEAD/Casks/corretto.rb
+# pinned to Java 11 instead of 16
+  version "11.0.10.9.1"
+  sha256 "4d3931e1be67c7a73a8f6c4820f29f954c168ac5b7154cd73ab4c38596d8266a"
+
+  url "https://corretto.aws/downloads/resources/#{version.sub(/-\d+/, "")}/amazon-corretto-#{version}-macosx-x64.pkg"
+  name "AWS Corretto JDK"
+  desc "OpenJDK distribution from Amazon"
+  homepage "https://corretto.aws/"
+
+  livecheck do
+    url "https://corretto.aws/downloads/latest/amazon-corretto-#{version.major}-x64-macos-jdk.pkg"
+    strategy :header_match do |headers|
+      headers["location"][%r{/amazon-corretto-(\d+(?:\.\d+)*)-macosx-x64\.pkg}i, 1]
+    end
+  end
+
+  pkg "amazon-corretto-#{version}-macosx-x64.pkg"
+
+  uninstall pkgutil: "com.amazon.corretto.#{version.major}"
+end
