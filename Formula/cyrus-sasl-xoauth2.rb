a575e4656255b5a7303b7ba929a1963ee6d4ed39
Evan Niessen-Derry <eniessenderry@gmail.com>
2026-03-06 17:54:36 -0600

    Set up build env for cyrus-sasl-xoauth2

--- Formula/cyrus-sasl-xoauth2.rb
+++ Formula/cyrus-sasl-xoauth2.rb
@@ -6,17 +6,24 @@ class CyrusSaslXoauth2 < Formula
     depends_on 'libtool' => :build
     depends_on 'autoconf' => :build
     depends_on 'automake' => :build
+    depends_on 'make' => :build
   end
 
   depends_on 'cyrus-sasl'
 
   def install
-    system '/usr/bin/sed', '-e', 's/^libtoolize/glibtoolize/', '-e', 's/^install/ginstall/', '-i', '', 'autogen.sh'
+    ENV.prepend_path "PATH", "#{Formula["libtool"].opt_libexec/gnubin}"
+    ENV.prepend_path "PATH", "#{Formula["make"].opt_libexec/gnubin}"
+    ENV.append "LDFLAGS", "-L#{Formula['cyrus-sasl'].opt_lib}"
+    ENV.append "CPPFLAGS", "-I#{Formula["cyrus-sasl"].opt_include}"
+    ENV.prepend_path "PKG_CONFIG_PATH", "#{Formula["cyrus-sasl"].opt_lib/pkgconfig}"
+
+    # system '/usr/bin/sed', '-e', 's/^libtoolize/glibtoolize/', '-e', 's/^install/ginstall/', '-i', '', 'autogen.sh'
     system './autogen.sh'
     system './configure',
            '--disable-silent-rules',
            "--prefix=#{prefix}",
-           "--with-cyrus-sasl=/opt/homebrew/opt/cyrus-sasl"
+           "--with-cyrus-sasl=#{Formula['cyrus-sasl'].opt_prefix}"
     system 'make', 'install'
   end
 end
