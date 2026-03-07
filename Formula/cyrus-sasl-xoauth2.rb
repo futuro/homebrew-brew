7d4278f37d1793e78d4183f7f6e05f0000ba55a3
Evan Niessen-Derry <eniessenderry@gmail.com>
2026-03-06 18:03:50 -0600

    fix string interpolation

--- Formula/cyrus-sasl-xoauth2.rb
+++ Formula/cyrus-sasl-xoauth2.rb
@@ -12,11 +12,11 @@ class CyrusSaslXoauth2 < Formula
   depends_on 'cyrus-sasl'
 
   def install
-    ENV.prepend_path "PATH", "#{Formula["libtool"].opt_libexec/gnubin}"
-    ENV.prepend_path "PATH", "#{Formula["make"].opt_libexec/gnubin}"
+    ENV.prepend_path "PATH", "#{Formula["libtool"].opt_libexec}/gnubin"
+    ENV.prepend_path "PATH", "#{Formula["make"].opt_libexec}/gnubin"
     ENV.append "LDFLAGS", "-L#{Formula['cyrus-sasl'].opt_lib}"
     ENV.append "CPPFLAGS", "-I#{Formula["cyrus-sasl"].opt_include}"
-    ENV.prepend_path "PKG_CONFIG_PATH", "#{Formula["cyrus-sasl"].opt_lib/pkgconfig}"
+    ENV.prepend_path "PKG_CONFIG_PATH", "#{Formula["cyrus-sasl"].opt_lib}/pkgconfig"
 
     # system '/usr/bin/sed', '-e', 's/^libtoolize/glibtoolize/', '-e', 's/^install/ginstall/', '-i', '', 'autogen.sh'
     system './autogen.sh'
